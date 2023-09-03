86bfefc6410640569e555603f2b2286a01dc1aad
Meerim K <thvrim@gmail.com>
2023-09-03 23:25:05 +0600

    в index.ts запустила сервер

---
+++ back/index.ts
@@ -0,0 +1,24 @@
+import express from 'express';
+import cors from 'cors';
+import * as mongoose from "mongoose";
+
+const app = express();
+const port = 8000;
+
+app.use(cors());
+app.use(express.static('public'));
+app.use(express.json());
+
+const run = async () => {
+    await mongoose.connect('mongodb://127.0.0.1:27017/spotify');
+
+    app.listen(port, () => {
+        console.log(`Server started on ${port} port!`);
+    });
+
+    process.on('exit', () => {
+        mongoose.disconnect();
+    });
+};
+
+run().catch(e => console.error(e));
