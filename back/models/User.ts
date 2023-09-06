fd54277f0cad9c7f0b4e56d3594d43f0478517d1
Meerim K <thvrim@gmail.com>
2023-09-06 22:54:39 +0600

    добавила хэширование пароля

--- back/models/User.ts
+++ back/models/User.ts
@@ -1,5 +1,8 @@
 import {model, Schema} from "mongoose";
 import {IUser} from "../types";
+import bcrypt from "bcrypt";
+
+const SALT_WORK_FACTOR = 10;
 
 const UserSchema = new Schema<IUser>({
     username: {
@@ -11,6 +14,23 @@ const UserSchema = new Schema<IUser>({
         type: String,
         required: true,
     }
+});
+
+UserSchema.pre('save', async function (next) {
+    if (!this.isModified('password')) return next();
+
+    const salt = await bcrypt.genSalt(SALT_WORK_FACTOR);
+    this.password = await bcrypt.hash(this.password, salt);
+
+    next();
+});
+
+UserSchema.set('toJSON', {
+   transform: (doc, ret, options) => {
+       delete ret.password;
+
+       return ret;
+   }
 });
 
 const User = model('User', UserSchema);
